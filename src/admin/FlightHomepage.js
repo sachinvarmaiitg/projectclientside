592d21aee1679f8316a7caccafdd80b948da89e2
Sachin <Sachinvarmaiitjee@gmail.com>
2024-06-10 00:50:10 +0530

    v5.7.9997

--- src/admin/FlightHomepage.js
+++ src/admin/FlightHomepage.js
@@ -12,7 +12,7 @@ let [flights,setflights]=useState([]);
 function formatISODate(isoDate) {
                 const newdate=new Date(isoDate);
                 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
-            return newdate.toISOString('en-US',options)
+            return newdate.toLocaleString('en-US',options)
     }
 let navigate = useNavigate();
     const getflights=()=>{
